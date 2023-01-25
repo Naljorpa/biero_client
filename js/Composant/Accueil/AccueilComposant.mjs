e167819c07a96f4160581e82e4ba52a3fa4c6647
Nicolas Fontaine <nikoniko.fontaine@gmail.com>
2023-01-25 17:55:29 -0500

    amelioration du style de l'accueil en ajoutant une grid de cartes de bieres

--- js/Composant/Accueil/AccueilComposant.mjs
+++ js/Composant/Accueil/AccueilComposant.mjs
@@ -1,25 +1,24 @@
 import ServiceBiere from "../../ServiceBiere.mjs";
 import Composant from "../Composant.mjs";
 
-export default class AccueilComposant extends Composant{
+export default class AccueilComposant extends Composant {
     template = "accueil";
     noeudParent = ""
     //pathGabarit ="/js/Composant/Accueil/accueil.html";
-    constructor(data, gabarit){
-        let pathGabarit ="./js/Composant/Accueil/accueil.html";
+    constructor(data, gabarit) {
+        let pathGabarit = "./js/Composant/Accueil/accueil.html";
         super(data, pathGabarit);
         this.noeudParent = document.querySelector(".app");
-        
+
         this.getBieres();
-        
 
     }
 
-    getBieres(){
+    getBieres() {
         ServiceBiere.getListeDesMeilleuresBieres(this.setData.bind(this));
         //ServiceBiere.getListeBieres(this.setData.bind(this));
-        
+
     }
 
-    
+
 }
